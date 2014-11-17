bd02a48967cef616bbfb410afb8f185dc8b76448
Andrew Gold <andrewmichaelgold@gmail.com>
2014-11-16 20:18:24 -0600

    Initial Commit

---
+++ SensoriSDK/SensoriSDK.h
@@ -0,0 +1,19 @@
+//
+//  SensoriSDK.h
+//  SensoriSDK
+//
+//  Created by Andrew Gold on 11/16/14.
+//  Copyright (c) 2014 Sensori, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SensoriSDK.
+FOUNDATION_EXPORT double SensoriSDKVersionNumber;
+
+//! Project version string for SensoriSDK.
+FOUNDATION_EXPORT const unsigned char SensoriSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SensoriSDK/PublicHeader.h>
+
+
